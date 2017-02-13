97f307ebcade184c294a1df16c2228ec9e203432
Piotrz <ruciof300@o2.pl>
2017-02-13 01:00:32 +0000

    backend started

--- learn-xamarin/learn_xamarin/Services/ExpendituresDataService.cs
+++ learn-xamarin/learn_xamarin/Services/ExpendituresDataService.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 using learn_xamarin.Model;
 using learn_xamarin.Storage;
 using learn_xamarin.Vm;
@@ -23,6 +28,47 @@ namespace learn_xamarin.Services
         public Expenditure[] GetAll()
         {
             return _localDatabase.GetAllExpenditures();
+        }
+    }
+
+    public class ExpendituresRestClient 
+    {
+        public ExpendituresRestClient()
+        {
+            
+        }
+
+        public void Add(Expenditure expenditure)
+        {
+            
+        }
+
+        private async void Post(string uri, object data)
+        {
+            var request = HttpWebRequest.Create(uri) as HttpWebRequest;
+            request.ContentType = "application/request";
+            request.Method = "POST";
+
+            Task<WebResponse> requestTask = Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, request.EndGetResponse, request);
+            using (var response = await requestTask)
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            sr.ReadLine(); //todo from hierrrr.
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void HandleIncomingResponse(IAsyncResult ar)
+        {
+            ar.
         }
     }
 }
